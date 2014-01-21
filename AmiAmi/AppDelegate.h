f7464e4941228a916c75e9e9292f8ddbbd5e8332
DaidoujiChen <daidoujichen@gmail.com>
2014-01-21 16:03:57 +0800

    Initial Commit

---
+++ AmiAmi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AmiAmi
+//
+//  Created by 啟倫 陳 on 2014/1/21.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
