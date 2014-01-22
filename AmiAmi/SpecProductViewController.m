0d61a574519d32ede1743acf84feaa9463961b2c
DaidoujiChen <daidoujichen@gmail.com>
2014-01-22 16:41:47 +0800

    modify blur effects, tune navigation performance

---
+++ AmiAmi/SpecProductViewController.m
@@ -0,0 +1,38 @@
+//
+//  SpecProductViewController.m
+//  AmiAmi
+//
+//  Created by 啟倫 陳 on 2014/1/22.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import "SpecProductViewController.h"
+
+@interface SpecProductViewController ()
+
+@end
+
+@implementation SpecProductViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
