48a9dac27a9983e991f55cb32700d2214f256766
DaidoujiChen <daidoujichen@gmail.com>
2014-03-03 16:29:36 +0800

    add uiviewcontroller swizzling, fix g+ share status bar not hidden issue

---
+++ AmiAmi/UIViewController+Swizzling.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Swizzling.h
+//  MaShuShuV2
+//
+//  Created by 啟倫 陳 on 2014/2/27.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Swizzling)
+
+@end
---
+++ AmiAmi/UIViewController+Swizzling.m
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Swizzling.m
+//  MaShuShuV2
+//
+//  Created by 啟倫 陳 on 2014/2/27.
+//  Copyright (c) 2014年 ChilunChen. All rights reserved.
+//
+
+#import "UIViewController+Swizzling.h"
+
+@implementation UIViewController (Swizzling)
+
++(void) load {
+    [self jr_swizzleMethod:@selector(prefersStatusBarHidden) withMethod:@selector(swizzling_prefersStatusBarHidden) error:nil];
+}
+
+-(BOOL) swizzling_prefersStatusBarHidden {
+    return YES;
+}
+
+@end
